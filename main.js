c13f85878155ba2be47148191090201458b7e377
Eddie Villareal <eddie.villareal27@gmail.com>
2018-12-19 15:59:40 -0800

    working on hard mode

--- main.js
+++ main.js
@@ -1,62 +1,83 @@
 $(document).ready(function(){
-	var filter = commonWords.filter(word => word.length >= 3);
+		var filter = commonWords.filter(word => word.length >= 3);
 
-	var random = filter[Math.floor(Math.random()*filter.length)];
+		var random = filter[Math.floor(Math.random()*filter.length)];
 
-	var answer = []
+		var answer = []
 
-	var attempt = 8
+		var attempt = 8
 
-	var attemptIn = document.querySelector("#attempt")
+		var attemptIn = document.querySelector("#attempt")
 
-		attemptIn.innerHTML = attempt
+			attemptIn.innerHTML = attempt
 
-	random.split('').forEach(word => answer.push('_'));
+		random.split('').forEach(word => answer.push('_'));
 
-	var placeIn = document.querySelector("#underscore")
+		var placeIn = document.querySelector("#underscore")
 
-		placeIn.innerHTML = answer
+			placeIn.innerHTML = answer
 
-	console.log(random)
-	console.log(answer)
-	
+		console.log(random)
+		console.log(answer)
+		
+		var used = []
 
-	$("#answertext").on("change", function(){
-			
-			
-			var playerAnswer = $("#answertext").val();
-			console.log(playerAnswer)
-
-			var counter = document.querySelector("#attempt")
-			counter.innerHTML = attempt--
-	
-			random.split('').forEach(function(e, i){
+		$("#answertext").on("change", function(){
 				
-				if(playerAnswer === e){
-				answer[i] = playerAnswer
-				placeIn.innerHTML = answer.join('')
-				}	
+				var letter = $("#answertext").val()
+				
+				used.push(letter)
 
-				if( answer.join('') === random ){
-			
-				document.querySelector("#status").innerHTML = "You win!"
-				$('#answertext').prop("readonly", true)
+				document.querySelector("#letterguess").innerHTML = used
 
+				console.log(used);
+
+				var playerAnswer = $("#answertext").val();
+				console.log(playerAnswer)
+
+				var counter = document.querySelector("#attempt")
+				counter.innerHTML = attempt--
+
+				if(attempt === 6){
+					$("#lifebar").css("width", "220")
+				} else if (attempt === 5){
+					$("#lifebar").css("width", "203")
+				} else if (attempt === 4){
+					$("#lifebar").css("width", "185")
+				} else if (attempt === 3){
+					$("#lifebar").css("width", "166")
+				} else if (attempt === 2){
+					$("#lifebar").css("width", "149")
+				} else if (attempt === 1){
+					$("#lifebar").css("width", "130")
+				} else if (attempt === 0){
+					$("#lifebar").css("width", "113")
+				} else if (attempt === -1){
+					$("#lifebar").css("width", "95")
 				} 
+		
+				random.split('').forEach(function(e, i){
+					
+					if(playerAnswer === e){
+					answer[i] = playerAnswer
+					placeIn.innerHTML = answer.join('')
+					}	
 
-				if (attempt === -1) {
-				document.querySelector("#status").innerHTML = "You lose!"
-				$('#answertext').prop("readonly", true)
+					if( answer.join('') === random ){
+				
+					document.querySelector("#status").innerHTML = "You win!"
+					$('#answertext').prop("readonly", true)
 
-			}
-			
+					} 
+
+					if (attempt === -1) {
+					document.querySelector("#status").innerHTML = "You lose!"
+					$('#answertext').prop("readonly", true)
+
+					}
+				
+				})
 			})
-
-
-
-
-
-		})
 
 })
 
